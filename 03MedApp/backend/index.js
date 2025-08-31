e2a93e1e258b6d4c1cee55488115bdc24baacf43
˜Kleuton <kleuton@msn.com>
2025-08-31 18:03:54 -0300

    cors

--- 03MedApp/backend/index.js
+++ 03MedApp/backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import pkg from "body-parser";
 import router from "./src/router/router.js";
 import "./src/database/database.js";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
@@ -10,6 +11,7 @@ const { json, urlencoded } = pkg;
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use("/", router);
+app.use(cors());
 
 app.listen(port, function () {
   console.log(`Listening to port ${port}`);
